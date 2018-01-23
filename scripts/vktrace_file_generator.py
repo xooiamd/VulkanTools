xooiamd/VulkanTools

commit d319d327a92a44577d60ed87795469356769bce4

vkreplay: GH365, Fix MAILBOX present mode support
This change fixes #365 "trace file with VK_PRESENT_MODE_MAILBOX_KHR
present mode is not replayed correctly".

It records the mapping between following objects:

* swapchain image <-> swapchain image index
* swapchain image view <-> swapchain image index
* swapchain framebuffer <-> swapchain image index

to replace:

* swapchain images in vkCmdWaitEvents and vkCmdPipelineBarrier
* swapchain framebuffers in vkCmdBeginRenderPass and vkBeginCommandBuffer

using the ones mapped to the image index returned by
vkAcquireNextImageKHR.

scripts/vktrace_file_generator.py
@@ -360,7 +360,7 @@ class VkTraceFileOutputGenerator(OutputGenerator):
             result = str(result).replace('::', '->')
         return result
     #
-    # Check if a structure is or contains a dispatchable (dispatchable = True) or 
+    # Check if a structure is or contains a dispatchable (dispatchable = True) or
     # non-dispatchable (dispatchable = False) handle
     def TypeContainsObjectHandle(self, handle_type, dispatchable):
         if dispatchable:
@@ -883,6 +883,16 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                    rr_string = rr_string.replace('pPacket->pFences', 'fences')
                 # Insert the real_*(..) call
                 replay_gen_source += '%s\n' % rr_string
+                if cmdname == 'DestroyFramebuffer':
+                    replay_gen_source += '            if (traceFramebufferToImageIndex.find(pPacket->framebuffer) != traceFramebufferToImageIndex.end()) {\n'
+                    replay_gen_source += '                traceImageIndexToFramebuffer.erase(traceFramebufferToImageIndex[pPacket->framebuffer]);\n'
+                    replay_gen_source += '                traceFramebufferToImageIndex.erase(pPacket->framebuffer);\n'
+                    replay_gen_source += '            }\n'
+                elif cmdname == 'DestroyImageView':
+                    replay_gen_source += '            if (traceImageViewToImageIndex.find(pPacket->imageView) != traceImageViewToImageIndex.end()) {\n'
+                    replay_gen_source += '                traceImageIndexToImageView.erase(traceImageViewToImageIndex[pPacket->imageView]);\n'
+                    replay_gen_source += '                traceImageViewToImageIndex.erase(pPacket->imageView);\n'
+                    replay_gen_source += '            }\n'
                 # Handle return values or anything that needs to happen after the real_*(..) call
                 get_ext_layers_proto = ['EnumerateInstanceExtensionProperties', 'EnumerateDeviceExtensionProperties','EnumerateInstanceLayerProperties', 'EnumerateDeviceLayerProperties']
                 if 'DestroyDevice' in cmdname:
@@ -904,6 +914,7 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                     replay_gen_source += '            }\n'
                 elif 'AcquireNextImage' in cmdname:
                     replay_gen_source += '            m_objMapper.add_to_pImageIndex_map(*(pPacket->pImageIndex), local_pImageIndex);\n'
+                    replay_gen_source += '            m_imageIndex = local_pImageIndex;\n'
                 elif 'DestroyInstance' in cmdname:
                     replay_gen_source += '            if (replayResult == VK_SUCCESS) {\n'
                     replay_gen_source += '                // TODO need to handle multiple instances and only clearing maps within an instance.\n'
@@ -941,6 +952,11 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                     replay_gen_source += '                m_objMapper.add_to_%ss_map(*(pPacket->%s), local_%s);\n' % (clean_type.lower()[2:], params[-1].name, params[-1].name)
                     if 'AllocateMemory' == cmdname:
                         replay_gen_source += '                m_objMapper.add_entry_to_mapData(local_%s, pPacket->pAllocateInfo->allocationSize);\n' % (params[-1].name)
+                    elif 'CreateImageView' == cmdname:
+                        replay_gen_source += '                if (traceImageToImageIndex.find(pPacket->pCreateInfo->image) != traceImageToImageIndex.end()) {\n'
+                        replay_gen_source += '                    traceImageIndexToImageView[traceImageToImageIndex[pPacket->pCreateInfo->image]] = *(pPacket->pView);\n'
+                        replay_gen_source += '                    traceImageViewToImageIndex[*(pPacket->pView)] = traceImageToImageIndex[pPacket->pCreateInfo->image];\n'
+                        replay_gen_source += '                }\n'
                     if ret_value:
                         replay_gen_source += '            }\n'
                 elif cmdname in do_while_dict:
@@ -2278,7 +2294,7 @@ class VkTraceFileOutputGenerator(OutputGenerator):
         # Assign packet values
         # FINISH packet
         # return result if needed
-       
+
         manually_written_hooked_funcs = ['vkAllocateCommandBuffers',
                                          'vkAllocateMemory',
                                          'vkAllocateDescriptorSets',
@@ -2499,7 +2515,7 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                 trace_vk_src += '    if (!g_trimEnabled) {\n'
                 # All buffers should be finalized by now, and the trace packet can be finished (which sends it over the socket)
                 trace_vk_src += '        FINISH_TRACE_PACKET();\n'
-       
+
                 # Else half of g_trimEnabled conditional
                 # Since packet wasn't sent to trace file, it either needs to be associated with an object, or deleted.
                 trace_vk_src += '    } else {\n'
@@ -2518,13 +2534,13 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                     trace_vk_src += '            vktrace_delete_trace_packet(&pHeader);\n'
                     trace_vk_src += '        }\n'
                 trace_vk_src += '    }\n'
-       
+
                 # Clean up instance or device data if needed
                 if proto.name == "vkDestroyInstance":
                     trace_vk_src += '    g_instanceDataMap.erase(key);\n'
                 elif proto.name == "vkDestroyDevice":
                     trace_vk_src += '    g_deviceDataMap.erase(key);\n'
-       
+
                 # Return result if needed
                 if 'void' not in resulttype or '*' in resulttype:
                     trace_vk_src += '    return result;\n'

vktrace/vktrace_replay/vkreplay_vkreplay.cpp
@@ -608,6 +608,10 @@ void vkReplay::manually_replay_vkDestroyImage(packet_vkDestroyImage *pPacket) {
     }
     m_vkDeviceFuncs.DestroyImage(remappedDevice, remappedImage, pPacket->pAllocator);
     m_objMapper.rm_from_images_map(pPacket->image);
+    if (traceImageToImageIndex.find(pPacket->image) != traceImageToImageIndex.end()) {
+        traceImageIndexToImage.erase(traceImageToImageIndex[pPacket->image]);
+        traceImageToImageIndex.erase(pPacket->image);
+    }
     if (replayGetImageMemoryRequirements.find(remappedImage) != replayGetImageMemoryRequirements.end())
         replayGetImageMemoryRequirements.erase(remappedImage);
     return;
@@ -1582,6 +1586,10 @@ void vkReplay::manually_replay_vkCmdWaitEvents(packet_vkCmdWaitEvents *pPacket) 
     VkImage *saveImg = VKTRACE_NEW_ARRAY(VkImage, pPacket->imageMemoryBarrierCount);
     for (idx = 0; idx < pPacket->imageMemoryBarrierCount; idx++) {
         VkImageMemoryBarrier *pNextImg = (VkImageMemoryBarrier *)&(pPacket->pImageMemoryBarriers[idx]);
+        if (traceImageToImageIndex.find(pNextImg->image) != traceImageToImageIndex.end() &&
+            traceImageIndexToImage.find(m_imageIndex) != traceImageIndexToImage.end() && m_imageIndex != UINT32_MAX) {
+            pNextImg->image = traceImageIndexToImage[m_imageIndex];
+        }
         saveImg[numRemapImg++] = pNextImg->image;
         traceDevice = traceImageToDevice[pNextImg->image];
         pNextImg->image = m_objMapper.remap_images(pNextImg->image);
@@ -1668,6 +1676,10 @@ void vkReplay::manually_replay_vkCmdPipelineBarrier(packet_vkCmdPipelineBarrier 
     }
     for (idx = 0; idx < pPacket->imageMemoryBarrierCount; idx++) {
         VkImageMemoryBarrier *pNextImg = (VkImageMemoryBarrier *)&(pPacket->pImageMemoryBarriers[idx]);
+        if (traceImageToImageIndex.find(pNextImg->image) != traceImageToImageIndex.end() &&
+            traceImageIndexToImage.find(m_imageIndex) != traceImageIndexToImage.end() && m_imageIndex != UINT32_MAX) {
+            pNextImg->image = traceImageIndexToImage[m_imageIndex];
+        }
         saveImg[numRemapImg++] = pNextImg->image;
         traceDevice = traceImageToDevice[pNextImg->image];
         if (traceDevice == NULL) vktrace_LogError("DEBUG: traceDevice is NULL");
@@ -1724,6 +1736,10 @@ VkResult vkReplay::manually_replay_vkCreateFramebuffer(packet_vkCreateFramebuffe
         pAttachments = VKTRACE_NEW_ARRAY(VkImageView, pInfo->attachmentCount);
         memcpy(pAttachments, pSavedAttachments, sizeof(VkImageView) * pInfo->attachmentCount);
         for (uint32_t i = 0; i < pInfo->attachmentCount; i++) {
+            if (traceImageViewToImageIndex.find(pInfo->pAttachments[i]) != traceImageViewToImageIndex.end()) {
+                traceFramebufferToImageIndex[*(pPacket->pFramebuffer)] = traceImageViewToImageIndex[pInfo->pAttachments[i]];
+                traceImageIndexToFramebuffer[traceImageViewToImageIndex[pInfo->pAttachments[i]]] = *(pPacket->pFramebuffer);
+            }
             pAttachments[i] = m_objMapper.remap_imageviews(pInfo->pAttachments[i]);
             if (pAttachments[i] == VK_NULL_HANDLE && pInfo->pAttachments[i] != VK_NULL_HANDLE) {
                 vktrace_LogError("Skipping vkCreateFramebuffer() due to invalid remapped VkImageView.");
@@ -1783,7 +1799,13 @@ void vkReplay::manually_replay_vkCmdBeginRenderPass(packet_vkCmdBeginRenderPass 
     VkRenderPassBeginInfo local_renderPassBeginInfo;
     memcpy((void *)&local_renderPassBeginInfo, (void *)pPacket->pRenderPassBegin, sizeof(VkRenderPassBeginInfo));
     local_renderPassBeginInfo.pClearValues = (const VkClearValue *)pPacket->pRenderPassBegin->pClearValues;
-    local_renderPassBeginInfo.framebuffer = m_objMapper.remap_framebuffers(pPacket->pRenderPassBegin->framebuffer);
+    if (traceFramebufferToImageIndex.find(pPacket->pRenderPassBegin->framebuffer) != traceFramebufferToImageIndex.end() &&
+        traceImageIndexToFramebuffer.find(m_imageIndex) != traceImageIndexToFramebuffer.end() && m_imageIndex != UINT32_MAX) {
+        // Use Framebuffer mapped to the image index returned by vkAcquireNextImage()
+        local_renderPassBeginInfo.framebuffer = m_objMapper.remap_framebuffers(traceImageIndexToFramebuffer[m_imageIndex]);
+    } else {
+        local_renderPassBeginInfo.framebuffer = m_objMapper.remap_framebuffers(pPacket->pRenderPassBegin->framebuffer);
+    }
     if (local_renderPassBeginInfo.framebuffer == VK_NULL_HANDLE) {
         vktrace_LogError("Skipping vkCmdBeginRenderPass() due to invalid remapped VkFramebuffer.");
         return;
@@ -1816,7 +1838,13 @@ VkResult vkReplay::manually_replay_vkBeginCommandBuffer(packet_vkBeginCommandBuf
         pRP = &(pHinfo->renderPass);
         pFB = &(pHinfo->framebuffer);
         *pRP = m_objMapper.remap_renderpasss(savedRP);
-        *pFB = m_objMapper.remap_framebuffers(savedFB);
+        if (traceFramebufferToImageIndex.find(savedFB) != traceFramebufferToImageIndex.end() &&
+            traceImageIndexToFramebuffer.find(m_imageIndex) != traceImageIndexToFramebuffer.end() && m_imageIndex != UINT32_MAX) {
+            // Use Framebuffer mapped to the image index returned by vkAcquireNextImage()
+            *pFB = m_objMapper.remap_framebuffers(traceImageIndexToFramebuffer[m_imageIndex]);
+        } else {
+            *pFB = m_objMapper.remap_framebuffers(savedFB);
+        }
     }
     replayResult = m_vkDeviceFuncs.BeginCommandBuffer(remappedCommandBuffer, pPacket->pBeginInfo);
     if (pInfo != NULL && pHinfo != NULL) {
@@ -3012,6 +3040,14 @@ void vkReplay::manually_replay_vkDestroySwapchainKHR(packet_vkDestroySwapchainKH
 
     m_vkDeviceFuncs.DestroySwapchainKHR(remappeddevice, remappedswapchain, pPacket->pAllocator);
     m_objMapper.rm_from_swapchainkhrs_map(pPacket->swapchain);
+
+    traceImageIndexToImage.clear();
+    traceImageToImageIndex.clear();
+    traceImageIndexToImageView.clear();
+    traceImageViewToImageIndex.clear();
+    traceImageIndexToFramebuffer.clear();
+    traceFramebufferToImageIndex.clear();
+    m_imageIndex = UINT32_MAX;
 }
 
 VkResult vkReplay::manually_replay_vkGetSwapchainImagesKHR(packet_vkGetSwapchainImagesKHR *pPacket) {
@@ -3037,6 +3073,8 @@ VkResult vkReplay::manually_replay_vkGetSwapchainImagesKHR(packet_vkGetSwapchain
         numImages = *(pPacket->pSwapchainImageCount);
         for (uint32_t i = 0; i < numImages; i++) {
             packetImage[i] = pPacketImages[i];
+            traceImageIndexToImage[i] = packetImage[i];
+            traceImageToImageIndex[packetImage[i]] = i;
             traceImageToDevice[packetImage[i]] = pPacket->device;
         }
     }

vktrace/vktrace_replay/vkreplay_vkreplay.h
@@ -92,6 +92,7 @@ class vkReplay {
     int m_frameNumber;
     vktrace_trace_file_header* m_pFileHeader;
     struct_gpuinfo* m_pGpuinfo;
+    uint32_t m_imageIndex = UINT32_MAX;
 
     // Replay platform description
     uint64_t m_replay_endianess;
@@ -256,6 +257,15 @@ class vkReplay {
     // Map VkPhysicalDevice to VkPhysicalDeviceMemoryProperites
     std::unordered_map<VkPhysicalDevice, VkPhysicalDeviceMemoryProperties> traceMemoryProperties;
     std::unordered_map<VkPhysicalDevice, VkPhysicalDeviceMemoryProperties> replayMemoryProperties;
+
+    // Map swapchain image index to VkImage, VkImageView, VkFramebuffer, so we can replace swapchain image and frame buffer with the
+    // acquired ones
+    std::unordered_map<uint32_t, VkImage> traceImageIndexToImage;
+    std::unordered_map<VkImage, uint32_t> traceImageToImageIndex;
+    std::unordered_map<uint32_t, VkImageView> traceImageIndexToImageView;
+    std::unordered_map<VkImageView, uint32_t> traceImageViewToImageIndex;
+    std::unordered_map<uint32_t, VkFramebuffer> traceImageIndexToFramebuffer;
+    std::unordered_map<VkFramebuffer, uint32_t> traceFramebufferToImageIndex;
 
     // Map VkImage to VkMemoryRequirements
     std::unordered_map<VkImage, VkMemoryRequirements> replayGetImageMemoryRequirements;

commit a33099263d62f40bc13f050b9d2d0cfb489ecd25

vktrace: put commit d319d32 back in
Next two commits will fix bug introduced by d319d32

Change-Id: I49f165926d755b633b2b6255fac3c241b3816aae

scripts/vktrace_file_generator.py
@@ -360,7 +360,7 @@ class VkTraceFileOutputGenerator(OutputGenerator):
             result = str(result).replace('::', '->')
         return result
     #
-    # Check if a structure is or contains a dispatchable (dispatchable = True) or 
+    # Check if a structure is or contains a dispatchable (dispatchable = True) or
     # non-dispatchable (dispatchable = False) handle
     def TypeContainsObjectHandle(self, handle_type, dispatchable):
         if dispatchable:
@@ -883,6 +883,16 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                    rr_string = rr_string.replace('pPacket->pFences', 'fences')
                 # Insert the real_*(..) call
                 replay_gen_source += '%s\n' % rr_string
+                if cmdname == 'DestroyFramebuffer':
+                    replay_gen_source += '            if (traceFramebufferToImageIndex.find(pPacket->framebuffer) != traceFramebufferToImageIndex.end()) {\n'
+                    replay_gen_source += '                traceImageIndexToFramebuffer.erase(traceFramebufferToImageIndex[pPacket->framebuffer]);\n'
+                    replay_gen_source += '                traceFramebufferToImageIndex.erase(pPacket->framebuffer);\n'
+                    replay_gen_source += '            }\n'
+                elif cmdname == 'DestroyImageView':
+                    replay_gen_source += '            if (traceImageViewToImageIndex.find(pPacket->imageView) != traceImageViewToImageIndex.end()) {\n'
+                    replay_gen_source += '                traceImageIndexToImageView.erase(traceImageViewToImageIndex[pPacket->imageView]);\n'
+                    replay_gen_source += '                traceImageViewToImageIndex.erase(pPacket->imageView);\n'
+                    replay_gen_source += '            }\n'
                 # Handle return values or anything that needs to happen after the real_*(..) call
                 get_ext_layers_proto = ['EnumerateInstanceExtensionProperties', 'EnumerateDeviceExtensionProperties','EnumerateInstanceLayerProperties', 'EnumerateDeviceLayerProperties']
                 if 'DestroyDevice' in cmdname:
@@ -904,6 +914,7 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                     replay_gen_source += '            }\n'
                 elif 'AcquireNextImage' in cmdname:
                     replay_gen_source += '            m_objMapper.add_to_pImageIndex_map(*(pPacket->pImageIndex), local_pImageIndex);\n'
+                    replay_gen_source += '            m_imageIndex = local_pImageIndex;\n'
                 elif 'DestroyInstance' in cmdname:
                     replay_gen_source += '            if (replayResult == VK_SUCCESS) {\n'
                     replay_gen_source += '                // TODO need to handle multiple instances and only clearing maps within an instance.\n'
@@ -941,6 +952,11 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                     replay_gen_source += '                m_objMapper.add_to_%ss_map(*(pPacket->%s), local_%s);\n' % (clean_type.lower()[2:], params[-1].name, params[-1].name)
                     if 'AllocateMemory' == cmdname:
                         replay_gen_source += '                m_objMapper.add_entry_to_mapData(local_%s, pPacket->pAllocateInfo->allocationSize);\n' % (params[-1].name)
+                    elif 'CreateImageView' == cmdname:
+                        replay_gen_source += '                if (traceImageToImageIndex.find(pPacket->pCreateInfo->image) != traceImageToImageIndex.end()) {\n'
+                        replay_gen_source += '                    traceImageIndexToImageView[traceImageToImageIndex[pPacket->pCreateInfo->image]] = *(pPacket->pView);\n'
+                        replay_gen_source += '                    traceImageViewToImageIndex[*(pPacket->pView)] = traceImageToImageIndex[pPacket->pCreateInfo->image];\n'
+                        replay_gen_source += '                }\n'
                     if ret_value:
                         replay_gen_source += '            }\n'
                 elif cmdname in do_while_dict:
@@ -2278,7 +2294,7 @@ class VkTraceFileOutputGenerator(OutputGenerator):
         # Assign packet values
         # FINISH packet
         # return result if needed
-       
+
         manually_written_hooked_funcs = ['vkAllocateCommandBuffers',
                                          'vkAllocateMemory',
                                          'vkAllocateDescriptorSets',
@@ -2499,7 +2515,7 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                 trace_vk_src += '    if (!g_trimEnabled) {\n'
                 # All buffers should be finalized by now, and the trace packet can be finished (which sends it over the socket)
                 trace_vk_src += '        FINISH_TRACE_PACKET();\n'
-       
+
                 # Else half of g_trimEnabled conditional
                 # Since packet wasn't sent to trace file, it either needs to be associated with an object, or deleted.
                 trace_vk_src += '    } else {\n'
@@ -2518,13 +2534,13 @@ class VkTraceFileOutputGenerator(OutputGenerator):
                     trace_vk_src += '            vktrace_delete_trace_packet(&pHeader);\n'
                     trace_vk_src += '        }\n'
                 trace_vk_src += '    }\n'
-       
+
                 # Clean up instance or device data if needed
                 if proto.name == "vkDestroyInstance":
                     trace_vk_src += '    g_instanceDataMap.erase(key);\n'
                 elif proto.name == "vkDestroyDevice":
                     trace_vk_src += '    g_deviceDataMap.erase(key);\n'
-       
+
                 # Return result if needed
                 if 'void' not in resulttype or '*' in resulttype:
                     trace_vk_src += '    return result;\n'

vktrace/vktrace_replay/vkreplay_vkreplay.cpp
@@ -608,6 +608,10 @@ void vkReplay::manually_replay_vkDestroyImage(packet_vkDestroyImage *pPacket) {
     }
     m_vkDeviceFuncs.DestroyImage(remappedDevice, remappedImage, pPacket->pAllocator);
     m_objMapper.rm_from_images_map(pPacket->image);
+    if (traceImageToImageIndex.find(pPacket->image) != traceImageToImageIndex.end()) {
+        traceImageIndexToImage.erase(traceImageToImageIndex[pPacket->image]);
+        traceImageToImageIndex.erase(pPacket->image);
+    }
     if (replayGetImageMemoryRequirements.find(remappedImage) != replayGetImageMemoryRequirements.end())
         replayGetImageMemoryRequirements.erase(remappedImage);
     return;
@@ -1582,6 +1586,10 @@ void vkReplay::manually_replay_vkCmdWaitEvents(packet_vkCmdWaitEvents *pPacket) 
     VkImage *saveImg = VKTRACE_NEW_ARRAY(VkImage, pPacket->imageMemoryBarrierCount);
     for (idx = 0; idx < pPacket->imageMemoryBarrierCount; idx++) {
         VkImageMemoryBarrier *pNextImg = (VkImageMemoryBarrier *)&(pPacket->pImageMemoryBarriers[idx]);
+        if (traceImageToImageIndex.find(pNextImg->image) != traceImageToImageIndex.end() &&
+            traceImageIndexToImage.find(m_imageIndex) != traceImageIndexToImage.end() && m_imageIndex != UINT32_MAX) {
+            pNextImg->image = traceImageIndexToImage[m_imageIndex];
+        }
         saveImg[numRemapImg++] = pNextImg->image;
         traceDevice = traceImageToDevice[pNextImg->image];
         pNextImg->image = m_objMapper.remap_images(pNextImg->image);
@@ -1668,6 +1676,10 @@ void vkReplay::manually_replay_vkCmdPipelineBarrier(packet_vkCmdPipelineBarrier 
     }
     for (idx = 0; idx < pPacket->imageMemoryBarrierCount; idx++) {
         VkImageMemoryBarrier *pNextImg = (VkImageMemoryBarrier *)&(pPacket->pImageMemoryBarriers[idx]);
+        if (traceImageToImageIndex.find(pNextImg->image) != traceImageToImageIndex.end() &&
+            traceImageIndexToImage.find(m_imageIndex) != traceImageIndexToImage.end() && m_imageIndex != UINT32_MAX) {
+            pNextImg->image = traceImageIndexToImage[m_imageIndex];
+        }
         saveImg[numRemapImg++] = pNextImg->image;
         traceDevice = traceImageToDevice[pNextImg->image];
         if (traceDevice == NULL) vktrace_LogError("DEBUG: traceDevice is NULL");
@@ -1724,6 +1736,10 @@ VkResult vkReplay::manually_replay_vkCreateFramebuffer(packet_vkCreateFramebuffe
         pAttachments = VKTRACE_NEW_ARRAY(VkImageView, pInfo->attachmentCount);
         memcpy(pAttachments, pSavedAttachments, sizeof(VkImageView) * pInfo->attachmentCount);
         for (uint32_t i = 0; i < pInfo->attachmentCount; i++) {
+            if (traceImageViewToImageIndex.find(pInfo->pAttachments[i]) != traceImageViewToImageIndex.end()) {
+                traceFramebufferToImageIndex[*(pPacket->pFramebuffer)] = traceImageViewToImageIndex[pInfo->pAttachments[i]];
+                traceImageIndexToFramebuffer[traceImageViewToImageIndex[pInfo->pAttachments[i]]] = *(pPacket->pFramebuffer);
+            }
             pAttachments[i] = m_objMapper.remap_imageviews(pInfo->pAttachments[i]);
             if (pAttachments[i] == VK_NULL_HANDLE && pInfo->pAttachments[i] != VK_NULL_HANDLE) {
                 vktrace_LogError("Skipping vkCreateFramebuffer() due to invalid remapped VkImageView.");
@@ -1783,7 +1799,13 @@ void vkReplay::manually_replay_vkCmdBeginRenderPass(packet_vkCmdBeginRenderPass 
     VkRenderPassBeginInfo local_renderPassBeginInfo;
     memcpy((void *)&local_renderPassBeginInfo, (void *)pPacket->pRenderPassBegin, sizeof(VkRenderPassBeginInfo));
     local_renderPassBeginInfo.pClearValues = (const VkClearValue *)pPacket->pRenderPassBegin->pClearValues;
-    local_renderPassBeginInfo.framebuffer = m_objMapper.remap_framebuffers(pPacket->pRenderPassBegin->framebuffer);
+    if (traceFramebufferToImageIndex.find(pPacket->pRenderPassBegin->framebuffer) != traceFramebufferToImageIndex.end() &&
+        traceImageIndexToFramebuffer.find(m_imageIndex) != traceImageIndexToFramebuffer.end() && m_imageIndex != UINT32_MAX) {
+        // Use Framebuffer mapped to the image index returned by vkAcquireNextImage()
+        local_renderPassBeginInfo.framebuffer = m_objMapper.remap_framebuffers(traceImageIndexToFramebuffer[m_imageIndex]);
+    } else {
+        local_renderPassBeginInfo.framebuffer = m_objMapper.remap_framebuffers(pPacket->pRenderPassBegin->framebuffer);
+    }
     if (local_renderPassBeginInfo.framebuffer == VK_NULL_HANDLE) {
         vktrace_LogError("Skipping vkCmdBeginRenderPass() due to invalid remapped VkFramebuffer.");
         return;
@@ -1816,7 +1838,13 @@ VkResult vkReplay::manually_replay_vkBeginCommandBuffer(packet_vkBeginCommandBuf
         pRP = &(pHinfo->renderPass);
         pFB = &(pHinfo->framebuffer);
         *pRP = m_objMapper.remap_renderpasss(savedRP);
-        *pFB = m_objMapper.remap_framebuffers(savedFB);
+        if (traceFramebufferToImageIndex.find(savedFB) != traceFramebufferToImageIndex.end() &&
+            traceImageIndexToFramebuffer.find(m_imageIndex) != traceImageIndexToFramebuffer.end() && m_imageIndex != UINT32_MAX) {
+            // Use Framebuffer mapped to the image index returned by vkAcquireNextImage()
+            *pFB = m_objMapper.remap_framebuffers(traceImageIndexToFramebuffer[m_imageIndex]);
+        } else {
+            *pFB = m_objMapper.remap_framebuffers(savedFB);
+        }
     }
     replayResult = m_vkDeviceFuncs.BeginCommandBuffer(remappedCommandBuffer, pPacket->pBeginInfo);
     if (pInfo != NULL && pHinfo != NULL) {
@@ -3012,6 +3040,14 @@ void vkReplay::manually_replay_vkDestroySwapchainKHR(packet_vkDestroySwapchainKH
 
     m_vkDeviceFuncs.DestroySwapchainKHR(remappeddevice, remappedswapchain, pPacket->pAllocator);
     m_objMapper.rm_from_swapchainkhrs_map(pPacket->swapchain);
+
+    traceImageIndexToImage.clear();
+    traceImageToImageIndex.clear();
+    traceImageIndexToImageView.clear();
+    traceImageViewToImageIndex.clear();
+    traceImageIndexToFramebuffer.clear();
+    traceFramebufferToImageIndex.clear();
+    m_imageIndex = UINT32_MAX;
 }
 
 VkResult vkReplay::manually_replay_vkGetSwapchainImagesKHR(packet_vkGetSwapchainImagesKHR *pPacket) {
@@ -3037,6 +3073,8 @@ VkResult vkReplay::manually_replay_vkGetSwapchainImagesKHR(packet_vkGetSwapchain
         numImages = *(pPacket->pSwapchainImageCount);
         for (uint32_t i = 0; i < numImages; i++) {
             packetImage[i] = pPacketImages[i];
+            traceImageIndexToImage[i] = packetImage[i];
+            traceImageToImageIndex[packetImage[i]] = i;
             traceImageToDevice[packetImage[i]] = pPacket->device;
         }
     }

vktrace/vktrace_replay/vkreplay_vkreplay.h
@@ -92,6 +92,7 @@ class vkReplay {
     int m_frameNumber;
     vktrace_trace_file_header* m_pFileHeader;
     struct_gpuinfo* m_pGpuinfo;
+    uint32_t m_imageIndex = UINT32_MAX;
 
     // Replay platform description
     uint64_t m_replay_endianess;
@@ -256,6 +257,15 @@ class vkReplay {
     // Map VkPhysicalDevice to VkPhysicalDeviceMemoryProperites
     std::unordered_map<VkPhysicalDevice, VkPhysicalDeviceMemoryProperties> traceMemoryProperties;
     std::unordered_map<VkPhysicalDevice, VkPhysicalDeviceMemoryProperties> replayMemoryProperties;
+
+    // Map swapchain image index to VkImage, VkImageView, VkFramebuffer, so we can replace swapchain image and frame buffer with the
+    // acquired ones
+    std::unordered_map<uint32_t, VkImage> traceImageIndexToImage;
+    std::unordered_map<VkImage, uint32_t> traceImageToImageIndex;
+    std::unordered_map<uint32_t, VkImageView> traceImageIndexToImageView;
+    std::unordered_map<VkImageView, uint32_t> traceImageViewToImageIndex;
+    std::unordered_map<uint32_t, VkFramebuffer> traceImageIndexToFramebuffer;
+    std::unordered_map<VkFramebuffer, uint32_t> traceFramebufferToImageIndex;
 
     // Map VkImage to VkMemoryRequirements
     std::unordered_map<VkImage, VkMemoryRequirements> replayGetImageMemoryRequirements;